6672667e172b29d28e18b0dd41a4c106f217cdc9
ivan.mamai00@mail.ru <250219As>
2021-06-07 17:03:42 +0300

    delete Autopark

--- src/Autopark/Level3/Level3.java
+++
@@ -1,7 +0,0 @@
-package Autopark.Level3;
-
-public class Level3 {
-    public static void main(String[] args) {
-        System.out.println(TechnicalSpecialist.validateManufactureYear(1700));
-    }
-}
--- src/Autopark/Level3/TechnicalSpecialist.java
+++
@@ -1,9 +0,0 @@
-package Autopark.Level3;
-
-public class TechnicalSpecialist {
-    public static final int LOWER_LIMIT_MANUFACTURE_YEAR = 1886;
-
-    static public boolean validateManufactureYear(int year) {
-        return year >= LOWER_LIMIT_MANUFACTURE_YEAR && new String(year + "").length() == 4;
-    }
-}
